2ae408642f8cd6b884ae4aa1c19e25438b805ea3
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-19 21:02:39 -0600

    primera version

---
+++ Ejercicio23.py
@@ -0,0 +1,8 @@
+# Hacer un programa donde se remplacen todos los espacios de una cadena por asteríscos y además cada palabra empiece por mayúsculas
+
+cadena = input("Digite una cadena: ")
+
+cadena = cadena.replace(" ",'*')
+print(cadena.title())
+
+#Carolina EM
